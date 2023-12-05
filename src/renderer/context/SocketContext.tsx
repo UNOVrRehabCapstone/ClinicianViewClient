98f377d2ed950acd93ea8c5d10175c2b0c44ef9d
EdwardMOMalley <edwardomalley@unomaha.edu>
2023-12-05 13:36:03 -0600

    new ip

--- src/renderer/context/SocketContext.tsx
+++ src/renderer/context/SocketContext.tsx
@@ -12,8 +12,16 @@ import { useSessionContext } from './SessionContext';
 import { IPatient } from 'renderer/interfaces/Session';
 import { IKRig } from 'renderer/components/PatientCard/Modals/IKRigModal';
 
+// Student server
 //const SERVER_IP = 'http://137.48.186.67:5000';
- const SERVER_IP = 'http://localhost:5000/';
+
+// Local
+ //const SERVER_IP = 'http://localhost:5000';
+
+ // AWS lightsail instance
+ const SERVER_IP = 'http://35.182.185.82:5000';
+
+ 
 export interface ISocketContext {
   sendMessage: (messageId: string, object: any) => void;
   connected: boolean;
